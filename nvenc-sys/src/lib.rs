02ed2960cd606ab0f768d2cd77dfa6028bcebe92
Kayo <kayo@giantcow.dev>
2025-08-08 23:41:40 +0000

    wip

--- nvenc-sys/src/lib.rs
+++ nvenc-sys/src/lib.rs
@@ -1,14 +1,33 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+pub mod ffi {
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+    /// Macro to generate per-structure version for use with API.
+    // Header definition:
+    // #define NVENCAPI_STRUCT_VERSION(ver) ((uint32_t)NVENCAPI_VERSION | ((ver)<<16) | (0x7 << 28))
+    pub const fn NVENCAPI_STRUCT_VERSION(ver: u32) -> u32 {
+        NVENCAPI_VERSION | ((ver << 16) | 0x7 << 28)
+    }
+
+    pub const NV_ENCODE_API_FUNCTION_LIST_VER: u32 = NVENCAPI_STRUCT_VERSION(2);
+    pub const NV_ENC_OPEN_ENCODE_SESSION_EX_PARAMS_VER: u32 = NVENCAPI_STRUCT_VERSION(1);
 }
 
 #[cfg(test)]
-mod tests {
+mod test {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn can_create_api_instance() {
+        unsafe {
+            let mut nv_func_list = std::mem::zeroed::<ffi::NV_ENCODE_API_FUNCTION_LIST>();
+            nv_func_list.version = ffi::NV_ENCODE_API_FUNCTION_LIST_VER;
+
+            let res = ffi::NvEncodeAPICreateInstance(&mut nv_func_list);
+            assert_eq!(ffi::_NVENCSTATUS_NV_ENC_SUCCESS, res);
+        }
     }
 }
--- nvenc/src/lib.rs
+++ nvenc/src/lib.rs
@@ -1,3 +1,22 @@
+use std::fmt::Display;
+
+pub mod nvenc_api;
+pub mod nvenc_session;
+
+pub struct NvEncApiError(u32);
+
+impl From<u32> for NvEncApiError {
+    fn from(value: u32) -> Self {
+        NvEncApiError(value)
+    }
+}
+
+impl Display for NvEncApiError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "NVENCSTATUS({})", self.0)
+    }
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
